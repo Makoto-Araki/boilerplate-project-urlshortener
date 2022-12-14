44646760ba28e4cadbeae7822944b3316c1aebbf
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2022-12-14 03:30:49 +0000

    2022年12月14日 - 02

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
+// Import & Config
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const url = require('./model/urllist');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const urlList = require('./model/urllist');
+
+// Secrets
+const user = process.env['user']
+const pass = process.env['pass']
+const cluster = process.env['cluster']
+const option1 = process.env['option1']
+const option2 = process.env['option2']
+const database = process.env['database']
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+// Mongo_URI
+const mongo_URI = `mongodb+srv://${user}:${pass}@${cluster}/${database}?${option1}`;
+
+// MongoDB Connect
+mongoose.connect(mongo_URI, option2)
+.then(() => {
+  console.log('Database connection successed')
+})
+.catch(err => {
+  console.error('Database connection failed')
+})
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
